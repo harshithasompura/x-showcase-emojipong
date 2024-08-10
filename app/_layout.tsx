3d2378698832d73b53de46bd25302a8ea5ccf724
h-sompura <sompharshi123@gmail.com>
2024-08-10 16:14:28 +0530

    re(navigation): seperate layout from index and set up stack provider

---
+++ app/_layout.tsx
@@ -0,0 +1,69 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import {
+  DMSans_400Regular,
+  DMSans_400Regular_Italic,
+  DMSans_700Bold,
+} from "@expo-google-fonts/dm-sans";
+
+import { useColorScheme } from "@/components/useColorScheme";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+export const unstable_settings = {
+  // Ensure that reloading on `/modal` keeps a back button present.
+  initialRouteName: "index",
+};
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    DMSans_Regular: DMSans_400Regular,
+    DMSans_Bold: DMSans_700Bold,
+    DMSans_Regular_Italic: DMSans_400Regular_Italic,
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </ThemeProvider>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,10 @@
-import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { useFonts } from "expo-font";
-import {
-  DMSans_400Regular,
-  DMSans_400Regular_Italic,
-  DMSans_700Bold,
-} from "@expo-google-fonts/dm-sans";
 
 export default function App() {
-  const [loaded, error] = useFonts({
-    DMSans_Regular: DMSans_400Regular,
-    DMSans_Bold: DMSans_700Bold,
-    DMSans_Regular_Italic: DMSans_400Regular_Italic,
-  });
-
-  useEffect(() => {
-    if (loaded || error) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded, error]);
-
-  if (!loaded && !error) {
-    return null;
-  }
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, World!</Text>
+      <Text style={styles.text}>Home Page</Text>
       <StatusBar style="auto" />
     </View>
   );
