d6cd4d6c4aaeb5aeb177ed89a9570273564a1e4e
h-sompura <sompharshi123@gmail.com>
2024-08-10 15:23:53 +0530

    re: add dm sans google fonts and update app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import {
+  DMSans_400Regular,
+  DMSans_400Regular_Italic,
+  DMSans_700Bold,
+} from "@expo-google-fonts/dm-sans";
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    DMSans_Regular: DMSans_400Regular,
+    DMSans_Bold: DMSans_700Bold,
+    DMSans_Regular_Italic: DMSans_400Regular_Italic,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Hello, World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontFamily: "DMSans_Regular",
+    fontSize: 18,
   },
 });
