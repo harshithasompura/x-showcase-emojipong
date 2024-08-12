390ec4a460818cdb79b3fa0e72b06c217c9a6a5f
h-sompura <sompharshi123@gmail.com>
2024-08-12 12:14:18 +0530

    wip(game): move around score text and close button to home

--- app/game.tsx
+++ app/game.tsx
@@ -1,26 +1,31 @@
 import { StyleSheet, Text, View, Pressable } from "react-native";
 import { Link } from "expo-router";
 import { useLocalSearchParams } from "expo-router";
-
+import { SafeAreaView } from "react-native-safe-area-context";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { useState } from "react";
 
 export default function Game() {
   const params = useLocalSearchParams();
   const { selectedEmoji } = params;
+  const [score, setScore] = useState(0);
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Game Screen</Text>
-      <Text>{selectedEmoji}</Text>
-      <Link href="/" asChild>
-        <Pressable style={styles.homeButton}>
-          <Text style={styles.homeButtonText}>Go to Home</Text>
-        </Pressable>
-      </Link>
+    <SafeAreaView style={styles.container}>
+      <View style={{ alignSelf: "flex-start", marginTop: -20 }}>
+        <Link href="/" asChild>
+          <Pressable style={styles.homeButton}>
+            <AntDesign name="close" size={40} color="#1C1A2F" />
+          </Pressable>
+        </Link>
+      </View>
+      <Text style={styles.score}>{score}</Text>
+      <Text style={{ fontSize: 56 }}>{selectedEmoji}</Text>
       <Link href="/score" asChild>
         <Pressable style={styles.scoreButton}>
           <Text style={styles.scoreButtonText}>See Score</Text>
         </Pressable>
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -29,23 +34,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#FECF23",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
   },
-  text: {
-    fontFamily: "DMSans_Regular",
-    fontSize: 18,
+  score: {
+    fontFamily: "DMSans_Bold",
+    fontSize: 80,
+    opacity: 0.3,
+    marginTop: 88,
   },
   homeButton: {
-    backgroundColor: "#0E0C22",
-    marginVertical: 24,
     paddingVertical: 20,
     paddingHorizontal: 48,
     borderRadius: 24,
-  },
-  homeButtonText: {
-    color: "#eee",
-    fontSize: 22,
-    fontFamily: "DMSans_Bold",
   },
   scoreButton: {
     backgroundColor: "#FE6E47",
